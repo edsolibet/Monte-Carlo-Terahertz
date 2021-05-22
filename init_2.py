d3ec14872bcc7e6e5b1ed22f76ae53b1c7a012f2
edsolibet <carlo.solibet@gmail.com>
2021-05-22 12:30:27 +0800

    Add files via upload
    fixed main and init files
    cleaned comments

---
+++ init_2.py
@@ -0,0 +1,214 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 21 09:19:34 2021
+
+@author: Carlo
+"""
+
+from __future__ import division
+import numpy as np
+import pandas as pd
+import random
+from scipy.stats import maxwell
+import matplotlib.pyplot as plt
+
+class const:
+    """
+    Constants used
+    """
+    inf = float('inf') # infinity
+    kT = 0.02585 # Boltzmann constant times the semiconductor temperature, expressed in eV
+    m_0 = 9.11E-31 # Electron mass in kg
+    q = 1.602E-19 # Electron charge in Coulombs
+    hbar = 1.055E-34 # Reduced planck's constant in Joules sec
+    hbar_eVs = 6.582E-16 # Reduced Planck's constant in eV sec
+    eps0 = 8.854E-12 # Vacuum permittivity
+    
+class Layer:
+    """
+    Layer of semiconductor with the following properties...
+    
+    matl = a material (an object with Material class)
+    
+    n_or_p = a string, either 'n', 'p' or 'i', for the doping polarity
+    
+    dope = density of dopants in cm^-3
+    
+    lz = thickness of the layer in nm
+    """
+    def __init__(self, matl, dope, lx, ly, lz):
+        self.matl = matl
+        self.dope = dope
+        self.lx = lx
+        self.ly = ly
+        self.lz = lz
+        
+def where_am_i(layers, dist):
+    """
+    distance is the distance from the start of layer 0.
+    
+    layers is a list of each layer; each element should be a Layer object.
+    
+    Return a dictionary {'current_layer':X, 'distance_into_layer':Y}.
+    (Note: X is a Layer object, not an integer index.)
+    """
+    if dist < 0:
+        raise ValueError('Point is outside all layers')
+    layer_index = 0
+    while layer_index <= (len(layers) - 1):
+        current_layer = layers[layer_index]
+        if dist <= current_layer.lz:
+            return {'current_layer': layer_index,
+                    'dist': dist}
+        else:
+            dist -= current_layer.lz
+            layer_index+=1
+    raise ValueError('Point is outside all layers. Distance = ' + str(dist))
+
+
+class GaAs:    
+    EG = 1.424 # energy bandgap
+    eps_0 = 13.1*const.eps0
+    eps_inf = 10.9*const.eps0
+    rho = 5360 
+    E_phonon = 0.03536  # phonon energy eV
+    vs = 5.22E3 # sound velocity (m/s)
+    dope = 1E16 # doping concentration
+    alpha = 1.1E6 # absorption coefficient
+    tot_scat = 1E15 # total scattering rate
+
+    # keys are valley index (0 = Gamma, 1 = L, 2 = X)
+    # electron relative mass
+    mass = {0: 0.063*const.m_0, 1: 0.170*const.m_0, 2: 0.58*const.m_0}
+    # Non parabolicity factor
+    nonparab = {0: 0.610, 1: 0.461, 2: 0.204}
+    # Acoustic Deformation Potential 
+    DA = {0: 7.01, 1: 9.2, 2: 9.0}
+    
+    # Potential energy difference between valleys (eV) 
+    E_val = {"GL": 0.29, "LG": 0.29, "GX": 0.48, "XG": 0.48, 
+              "LX": 0.19, "XL": 0.19}
+    # Equivalent final valleys
+    B = {0: 1, 1: 4, 2: 3}
+    
+    # Deformation potential eV/cm
+    defpot = {"GL": 1.8E8, "LG": 1.8E8, "GX": 10E8, "XG": 10E8, 
+              "LX": 1E8, "XL": 1E8, "LL": 1E8, "XX": 10E8}
+    
+    # Phonon energies (eV)
+    EP = {"GL": 0.0278, "LG": 0.0278, "GX": 0.0299, "XG": 0.0299, 
+              "LX": 0.0293, "XL": 0.0293, "LL": 0.029, "XX": 0.0299}
+
+def init_energy_df(max_nrg = 2, div = 10000):
+    ''' Generates energy discretization database
+    Inputs:
+        max_nrg : maximum energy in eV
+        div : number of divisions/resolution of energy
+    '''
+    return pd.DataFrame(np.linspace(0, max_nrg, div), columns = ['Ek'])
+
+def inv_debye_len(layer):
+    ''' Calculates the inverse debye length
+    
+    Parameters
+    ----------
+    layer : class
+        material layer class
+
+    Returns
+    -------
+    float
+        inverse debye length
+    '''
+    return np.sqrt(const.q * layer.dope * 1E2**3 / (layer.matl.eps_0 * const.kT))
+
+class Device:
+    ''' Simulation parameters '''
+    dt = 2E-15 # time step
+    pts = 500 # number of time intervals
+    
+    ''' --- Device geometry and material composition --- '''
+
+    layer0 = Layer(matl=GaAs, dope=1E16, lx = 1E-6, ly = 1E-6, lz=10E-6)
+    layer1 = Layer(matl=GaAs, dope=1E16, lx = 1E-6, ly = 1E-6, lz=10E-6)
+    layers = [layer0]
+    Materials = [layer.matl for layer in layers]
+    
+    # number of particles
+    num_carr = 1000
+    #electric field
+    elec_field = np.array([0, 0, -10000]) # Ex, Ey, Ez (V/cm)
+    # initial mean energy of particles xkT
+    mean_energy = 10
+    # max energy (eV)
+    max_nrg = 2
+    # energy divisions
+    div = 10000
+    # energy db
+    dfEk = init_energy_df(max_nrg, div)
+    #dimensions of device
+    dim = [np.array([layer.lx, layer.ly, layer.lz]) for layer in layers]
+    
+def init_coords(layers = Device.layers, num_carr = Device.num_carr, mean_nrg = Device.mean_energy):
+    #initialize positions (x, y, z)
+    x = np.random.uniform(0, np.max(Device.dim, axis = 0)[0], int(num_carr))
+    y = np.random.uniform(0, np.max(Device.dim, axis = 0)[1], int(num_carr))
+    z = np.random.exponential(1/layers[0].matl.alpha, int(num_carr))
+
+    # initialize wave vectors, x1E9 m^-1
+    # print ("Initializing initial energy (eV).")
+    mean, scale = mean_nrg, 1E-7
+    e = maxwell.rvs(loc = mean * const.kT, scale = scale, size = num_carr)
+    
+    # print ("Initializing initial wave vectors (m^-1).")
+    kx = []
+    ky = []
+    kz = []
+    for ndx, i in enumerate(e):
+        mat = layers[where_am_i(layers, z[ndx])['current_layer']].matl
+        k = np.sqrt(2*mat.mass[0] * const.q * i/const.hbar**2) 
+        alpha = np.random.uniform(0, 2*np.pi)
+        beta = np.random.uniform(0, 2*np.pi)
+        kx.append(k*np.cos(alpha)*np.sin(beta))
+        ky.append(k*np.sin(alpha)*np.sin(beta))
+        kz.append(k*np.cos(beta))      
+        
+    coords = np.zeros((num_carr,6))
+    # remove for loop
+    for i in range(num_carr):
+        coords[i][0] = kx[i]
+        coords[i][1] = ky[i]
+        coords[i][2] = kz[i]
+        coords[i][3] = x[i]
+        coords[i][4] = y[i]
+        coords[i][5] = z[i]    
+    return coords
+
+def calc_energy(k, val, mat):
+    '''
+    Calculates the energy from the given wavevector coordinates k, valley, and material
+    Converts the calculated energy to its nonparabolic equivalent
+    Searches for the closest energy value in the preconstructed energy discretization
+    
+    k : list of kx, ky, kz coordinates of a particle
+    val : valley assignment of particle
+    mat : material class
+    
+    return : (1) float, nonparabolic energy value found within energy database
+             (2) int, index of energy in energy database
+    '''
+    E = sum(np.square(k))*(const.hbar)**2/(2*mat.mass[val]*const.q)
+    if np.isnan(E):
+        raise Exception('NaN error: NaN value detected')
+    return E
+
+def show_nrg_pos(coords, mat):
+    val = np.zeros(Device.num_carr).astype(int)
+    nrg = [calc_energy(coords[i][:3], val[i], mat) for i in range(Device.num_carr)]
+    plt.plot(coords[:, -1], nrg, 'o')
+    plt.ylabel("Energy")
+    plt.xlabel("Position")
+
+
+
+
---
+++ main_2.py
@@ -0,0 +1,341 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 23:12:22 2021
+
+@author: Carlo
+"""
+
+from __future__ import division
+
+import datetime
+import logging
+
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(levelname)s \
+                    - %(message)s')
+logging.debug('Start of Program')
+startTime = datetime.datetime.now()
+
+import numpy as np
+import matplotlib.pyplot as plt
+import init_2, scatter_
+import os, openpyxl
+
+const, dev = init_2.const, init_2.Device
+mat = dev.Materials
+dfEk = dev.dfEk
+logging.debug('Parameters loaded')
+
+''' --- Conditions --- '''
+scat_cond = True
+save_cond = False
+drift_cond = True
+free_flight_cond = False
+
+''' --- Initialization --- '''
+# ini_coords: 1 x 6 x num_carr arrays for kx, ky, kz, x, y, z v
+ini_coords = init_2.init_coords()
+coords = np.copy(ini_coords)
+valley = np.zeros(dev.num_carr).astype(int)
+
+
+if scat_cond:
+    scat_tables = [scatter_.calc_scatter_table(dfEk, ndx) for ndx in range(len(dev.layers))] # Need scatter tables for each material
+if len(ini_coords) == len(valley) and len(ini_coords) >0:
+    logging.debug('Initial coordinates and valley assignments generated.')
+
+# Generate Excel file
+dirPath = 'C:/Users/Carlo/OneDrive/Research/Terahertz/Python Codes/MC Simulation/'
+folderName = 'Simulation Results'
+os.chdir(dirPath + folderName)
+keyword = "Steady-state Transport Kernel"
+
+
+''' --- Functions --- '''
+
+def calc_energy(k, val, mat):
+    '''
+    Calculates the energy from the given wavevector coordinates k, valley, and material
+    Searches for the closest energy value in the preconstructed energy discretization
+    
+    k : list of kx, ky, kz coordinates of a particle
+    val : valley assignment of particle
+    mat : material class
+    
+    return : (1) float, nonparabolic energy value found within energy database
+             (2) int, index of energy in energy database
+    '''
+    E = sum(np.square(k))*(const.hbar)**2/(2*mat.mass[val]*const.q)
+    if np.isnan(E):
+        raise Exception('NaN error: NaN value detected')
+    ndx = (dfEk - E).abs().idxmin()
+    return E, ndx.iloc[0]
+    
+def free_flight(G0 = 3E14):
+    '''
+    Calculates a random free flight duration based on the given total scattering rate G0
+    
+    G0 : total scattering rate; must be greater than calculated maximum scattering rate 
+    
+    return: float, free flight duration of single particle
+    '''
+    r1 = np.random.uniform(0, 1)
+    return (-1/G0)*np.log(r1)
+
+
+def where_am_i(layers, dist):
+    '''
+    distance is the distance from the start of layer 0.
+    
+    layers is a list of each layer; each element should be a Layer object.
+    
+    Return a dictionary {'current_layer':X, 'distance_into_layer':Y}.
+    (Note: X is a Layer object, not an integer index.)
+    '''
+    d = dist
+    if d < 0:
+        raise ValueError('Point is before all layers. Distance = ' + str(dist))
+    layer_index = 0
+    while layer_index <= (len(layers) - 1):
+        current_layer = layers[layer_index]
+        if d <= current_layer.lz:
+            return {'current_layer': layer_index,
+                    'dist': d}
+        else:
+            d -= current_layer.lz
+            layer_index+=1
+    raise ValueError('Point is after all layers. Distance = ' + str(dist))
+
+def specular_refl(k, r, dim):
+    ''' Calculates the z- components of position and wavevector when specular reflection occurs
+    
+    k : wavevectors
+    r : positions
+    dim : total device dimensions
+    
+    return (1) : array, updated z- wavevector
+           (2) : array, updated z- position
+    '''
+    #needs total dim instead of material dim
+    while not (r[2] >= 0 and r[2] <= dim[2]):
+        k[2] = -1*np.bool(r[2] > dim[2] or r[2] < 0)*k[2] + np.bool(r[2] > 0 and r[2] < dim[2])*k[2]
+        r[2] = 2*dim[2]*np.bool(r[2] >= dim[2]) + -1*np.bool(r[2] >= dim[2] or r[2] <= 0)*r[2] + np.bool(r[2] > 0 and r[2] < dim[2])*r[2]   
+    return k, r
+
+def carrier_drift(coord, elec_field, dt2, mass):
+    '''
+    Performs the carrier drift sequence for each set of coordinates in a given electric field
+    at a particular time duration dt2 of each particle
+    Takes into account the mass of the particle in a given valley
+    
+    coord : wavevector and position coordinates (kx, ky, kz, x, y, z)
+    elec_field : electric field magnitudes in each direction (V/cm)
+    dt2 : drift duration
+    mass: particle mass in a given valley
+    
+    return: list, updated coordinates (kx, ky, kz, x, y, z)
+    '''
+    
+    k = coord[:3]
+    r = coord[3:]
+    
+    k += - const.q*dt2*elec_field*1e2/const.hbar # elec field in V/m
+    r += (const.hbar*k - 0.5*const.q*elec_field*1e2*dt2)*(dt2/mass)
+    #ndx = where_am_i(dev.layers, r_[2])['current_layer']
+    # checks if cyclic boundary conditions are satisfied
+    #r_ = cyclic_boundary(r_, dev.tot_dim)
+    #checks if specular reflection is satisfied
+    #tot_dim = np.append(np.max(dev.dim, axis = 0)[:2], np.sum(dev.dim, axis = 0)[2])
+    k, r = specular_refl(k, r, dev.dim[0])
+    
+    coord[:3] = k
+    coord[3:] = r
+    
+    # E = sum(np.square(k_))*(hbar)**2/(2*mat.mass[val]*q)
+    # E = 2*E/(1 + np.sqrt(1 + mat.nonparab[val]*E))
+    
+    
+    # Value check
+    if True in r > 1 or True in r < 0:
+        raise Exception('Invalid position coordinates')
+    if True in np.abs(k) < 1e5:
+        raise Exception('Invalid wavevector coordinates')
+    return coord
+
+
+    
+''' --- Main Monte Carlo Transport Sequence --- '''
+if drift_cond:
+    # Generate quantity list placeholders num_carr (rows) x pts (columns)
+    t_range = [i * dev.dt for i in range(dev.pts)]
+    v_hist = np.zeros((dev.num_carr, dev.pts))
+    x_hist = np.zeros((dev.num_carr, dev.pts))
+    y_hist = np.zeros((dev.num_carr, dev.pts))
+    z_hist = np.zeros((dev.num_carr, dev.pts))
+    e_hist = np.zeros((dev.num_carr, dev.pts))
+    val_hist = np.zeros((dev.num_carr, dev.pts))
+
+    # Generate initial free flight durations for all particles
+    dtau = [free_flight(dev.Materials[0].tot_scat) for i in range(dev.num_carr)]
+
+    for c, t in enumerate(t_range):
+        logging.debug('Time: %0.4g' %t)
+        # Start transport sequence, iterate over each particle
+        for carr in range(dev.num_carr):
+            dte = dtau[carr]
+            # time of flight longer than time interval dt
+            if dte >= dev.dt:
+                dt2 = dev.dt
+                # get elec field at coordinate
+                mat_i = where_am_i(dev.layers, coords[carr][5])['current_layer']
+                drift_coords = carrier_drift(coords[carr], dev.elec_field, dt2, mat[mat_i].mass[int(valley[carr])])
+            # time of flight shorter than time interval dt
+            else:
+                dt2 = dte
+                # get elec field at coordinate
+                mat_i = where_am_i(dev.layers, coords[carr][5])['current_layer']
+                drift_coords = carrier_drift(coords[carr], dev.elec_field, dt2, mat[mat_i].mass[int(valley[carr])])
+                # iterate free flight until approaching dt
+                while dte < dev.dt:
+                    dte2 = dte
+                    mat_i = where_am_i(dev.layers, drift_coords[5])['current_layer']
+                    if (1-free_flight_cond): # use scattering tables or free flight only
+                        drift_coords, valley[carr] = scatter_.scatter(drift_coords, scat_tables[mat_i][int(valley[carr])], int(valley[carr]), dfEk, mat_i)
+                    dt3 = free_flight(mat[mat_i].tot_scat)
+                    # Calculate remaining time dtp before end of interval dt
+                    dtp = dev.dt - dte2
+                    if dt3 <= dtp: # free flight after scattering is less than dtp
+                        dt2 = dt3
+                    else: # free flight after scattering is longer than dtp
+                        dt2 = dtp
+                    # get elec field at coordinate
+                    mat_i = where_am_i(dev.layers, drift_coords[5])['current_layer']
+                    drift_coords = carrier_drift(drift_coords, dev.elec_field, dt2, mat[mat_i].mass[int(valley[carr])])
+                    dte = dte2 + dt3
+            dte -= dev.dt
+            dtau[carr] = dte
+            coords[carr] = drift_coords
+            mat_i = where_am_i(dev.layers, drift_coords[5])['current_layer']
+            e_hist[carr][c] = calc_energy(drift_coords[:3], int(valley[carr]), mat[mat_i])[0]
+#            v_hist[carr][c] = drift_coords[2]*const.hbar/mat[mat_i].mass[int(valley[carr])]
+            v_hist[carr][c] = np.sqrt(2*const.q*e_hist[carr][c]/mat[mat_i].mass[int(valley[carr])])
+            z_hist[carr][c] = drift_coords[5]
+            val_hist[carr][c] = valley[carr]
+        
+    ''' --- Plots --- '''
+    fig1, ax1 = plt.subplots()
+    ax1.plot(np.array(t_range)*1E12, v_hist.mean(axis=0))
+    ax1.set_xlabel('Time (ps)')
+    ax1.set_ylabel('Mean Velocity (m/s)')
+    ax1.set_xlim([0, t_range[-1]*1E12])
+    
+    fig2, ax2 = plt.subplots()
+    ax2.plot(np.array(t_range)*1E12, e_hist.mean(axis = 0))
+    ax2.set_xlabel('Time (ps)')
+    ax2.set_ylabel('Mean Energy (eV)')
+    ax2.set_xlim([0, t_range[-1]*1E12])
+
+    #val_hist_ = [val_hist[:, i].tolist().count(j) for i, j in product(range(dev.pts), range(3))]
+    
+    G_val = np.zeros(dev.pts)
+    L_val = np.zeros(dev.pts)
+    X_val = np.zeros(dev.pts)
+    for i in range(dev.pts):
+        G_val[i] = val_hist[:,i].tolist().count(0)
+        L_val[i] = val_hist[:,i].tolist().count(1)
+        X_val[i] = val_hist[:,i].tolist().count(2)
+    
+    fig3, ax3 = plt.subplots()
+    ax3.plot(np.array(t_range)*1E12, G_val, label = r"$\Gamma$ pop.")
+    ax3.plot(np.array(t_range)*1E12, L_val, label = r"L pop.")
+    ax3.plot(np.array(t_range)*1E12, X_val, label = r"X pop.")
+    ax3.set_xlabel('Time (ps)')
+    ax3.set_ylabel('Valley population')
+    ax3.set_xlim([0, t_range[-1]*1E12])
+    
+    if save_cond:
+        num = 1
+        while True:
+            filename = datetime.datetime.today().strftime("%Y-%m-%d") + ' ' + keyword + \
+            ' (' + str(num) + ').xlsx'
+            if not os.path.exists(filename):
+                break
+            num += 1
+        ax1.savefig(filename)
+        ax2.savefig(filename)
+        ax3.savefig(filename)
+    
+    fig4, ax4 = plt.subplots()
+#    fin_nrg = [calc_energy(coords[i,:3], valley[i], mat[where_am_i(dev.layers, coords[i][5])['current_layer']])[0]] 
+#        for i in range(dev.num_carr)]
+    ax4.plot(coords[:,5], e_hist[:, -1], 'o')
+    ax4.set_xlabel("z-axis")
+    ax4.set_ylabel("Energy, eV")
+#    ax4.set_ylim([0, 1])
+    
+
+''' --- End Simulation --- '''
+endTime = datetime.datetime.now()
+total_time = endTime - startTime
+mins = int(total_time.total_seconds() / 60)
+secs = total_time.total_seconds() % 60
+print("---%s minutes, %s seconds ---" % (mins, np.round(secs, 3)))
+print ("Time finished: ", endTime.strftime("%d-%m-%Y %H:%M:%S"))
+
+
+
+def save_results():
+    
+    num = 1
+    while True:
+        filename = datetime.datetime.today().strftime("%Y-%m-%d") + ' ' + keyword + \
+        ' (' + str(num) + ').xlsx'
+        if not os.path.exists(filename):
+            break
+        num += 1
+    wb = openpyxl.Workbook()
+    wb.save(os.getcwd() + '\\' + filename)
+    wb = openpyxl.load_workbook(os.getcwd() + '\\' + filename)
+    sheet = wb[wb.sheetnames[0]]
+    for c, t in enumerate(t_range):
+        sheet['A' + str(c + 2)] = t*1E12
+        sheet['B' + str(c+2)] = np.mean(z_hist[:,c])*1E9
+        sheet['C' + str(c+2)] = np.mean(v_hist[:,c])
+        sheet['D' + str(c+2)] = np.mean(e_hist[:,c])
+        sheet['E' + str(c+2)] = G_val[c]
+        sheet['F' + str(c+2)] = L_val[c]
+        sheet['G' + str(c+2)] = X_val[c]
+        
+    ''' --- Excel Workbook Inputs --- '''
+        
+    xl_input = {'Number of Carriers' : dev.num_carr,
+                'Electric field (V/cm)': dev.elec_field[2],
+                'Impurity Doping (cm-3)': dev.layers[0].matl.dope,
+                'Time step (ps)': dev.dt,
+                'Data Points': dev.pts,
+                'Simulation Time (ps)': dev.dt*dev.pts
+                }
+    
+    # Series Heading titles
+    sheet['A1'] = 'Time (ps)'
+    sheet['B1'] = 'Average z pos. (nm)'
+    sheet['C1'] = 'Average velocity (m/s)'
+    sheet['D1'] = 'Average energy (eV)'
+    sheet['E1'] = 'Gamma-Valley Population'
+    sheet['F1'] = 'L-Valley Population'
+    sheet['G1'] = 'X-Valley Population'
+    
+    for i, key in enumerate(list(xl_input.keys())):
+        sheet['I' + str(i+2)] = key
+        sheet['J' + str(i+2)] = xl_input[key]
+    sheet['I' + str(len(xl_input.keys()) + 1)] = 'Actual Simulation Time'
+    sheet['J' + str(len(xl_input.keys()) + 1)] = f'{mins} mins, {secs:.2f} s'
+    
+    
+    # Set column width and freeze first row
+    sheet.column_dimensions['I'].width = 21
+    sheet.freeze_panes = 'A2'
+    wb.save(os.getcwd() + '\\' + filename)
+    
+
+if save_cond:
+   save_results()
